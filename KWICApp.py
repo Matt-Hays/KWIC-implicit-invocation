af5f13c618a73134dfaebd6b9069bfb0d7427b7c
MattHays2 <mhays@distorted.dev>
2025-05-14 18:22:31 -0600

    Remove query methods (used for testing)

--- KWICApp.py
+++ KWICApp.py
@@ -3,8 +3,8 @@ from LineStorage import LineStorage
 from CircularShifter import CircularShifter
 from Alphabetizer import Alphabetizer
 from IndexPrinter import IndexPrinter
-from QueryService import QueryService
-from typing import List
+# from QueryService import QueryService
+# from typing import List
 
 class KWICApp:
     def __init__(self):
@@ -13,11 +13,11 @@ class KWICApp:
         self._shifter = CircularShifter(self._bus, self._repo)
         Alphabetizer(self._bus, self._repo)
         IndexPrinter(self._bus, self._repo)
-        self._query = QueryService(self._repo, self._shifter.shift_map)
+        # self._query = QueryService(self._repo, self._shifter.shift_map)
         
     def add_line(self, text: str) -> None:
         line_id = self._repo.add_line(text)
         self._bus.publish("line_added", line_id=line_id, text=text)
         
-    def find(self, keyword: str) -> List[str]:
-        return self._query.search(keyword)
+    # def find(self, keyword: str) -> List[str]:
+    #     return self._query.search(keyword)
--- main.py
+++ main.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     kwic.add_line("The quick brown fox")
     kwic.add_line("Jumped over the lazy dog")
     
-    search_term = "fox"
-    hits = kwic.find(search_term)
-    print(f"\nQUERY '{search_term} -> {hits}")
+    # search_term = "fox"
+    # hits = kwic.find(search_term)
+    # print(f"\nQUERY '{search_term} -> {hits}")
